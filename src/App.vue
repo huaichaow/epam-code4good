3ed57a50786a574d935b8093cf01adecb105ef56
Huaichao Wang <hchwang001@163.com>
2018-11-17 11:56:23 +0800

    change layout

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,42 @@
 <template>
   <div id="app">
     <div id="nav">
-      <div class="nav-item back"></div>
-      <div class="nav-item title">{{title}}</div>
-      <div class="nav-item menu"></div>
+      <div class="center-fixed">
+        <div class="nav-item back"></div>
+        <div class="nav-item title">{{title}}</div>
+        <div class="nav-item menu"></div>
+      </div>
     </div>
     <div id="content">
       <router-view @setTitle="setTitle"/>
     </div>
     <div id="foot">
-      <router-link to="/" class="menu-item">
-        <div>
-          <div class="icon" style="background-image: url(/img/icon_tab_bar2@3x.png)"></div>
-          <div class="text">首页</div>
-        </div>
-      </router-link>
-      <router-link to="/about" class="menu-item">
-        <div>
-          <div class="icon" style="background-image: url(/img/icon_tab_bar1@3x.png)"></div>
-          <div class="text">附件</div>
-        </div>
-      </router-link>
-      <router-link to="/register" class="menu-item">
-        <div>
-          <div class="icon" style="background-image: url(/img/icon_tab_bar3@3x.png)"></div>
-          <div class="text">通知我</div>
-        </div>
-      </router-link>
-      <router-link to="/user" class="menu-item">
-        <div>
-          <div class="icon" style="background-image: url(/img/icon_tab_bar4@3x.png)"></div>
-          <div class="text">我</div>
-        </div>
-      </router-link>
+      <div class="center-fixed">
+        <router-link to="/" class="menu-item">
+          <div>
+            <div class="icon" style="background-image: url(/img/icon_tab_bar2@3x.png)"></div>
+            <div class="text">首页</div>
+          </div>
+        </router-link>
+        <router-link to="/about" class="menu-item">
+          <div>
+            <div class="icon" style="background-image: url(/img/icon_tab_bar1@3x.png)"></div>
+            <div class="text">附件</div>
+          </div>
+        </router-link>
+        <router-link to="/register" class="menu-item">
+          <div>
+            <div class="icon" style="background-image: url(/img/icon_tab_bar3@3x.png)"></div>
+            <div class="text">通知我</div>
+          </div>
+        </router-link>
+        <router-link to="/user" class="menu-item">
+          <div>
+            <div class="icon" style="background-image: url(/img/icon_tab_bar4@3x.png)"></div>
+            <div class="text">我</div>
+          </div>
+        </router-link>
+    </div>
     </div>
   </div>
 </template>
@@ -47,7 +51,6 @@ export default {
   },
   methods: {
     setTitle(title) {
-      console.log(title);
       this.title = title || "";
     }
   }
