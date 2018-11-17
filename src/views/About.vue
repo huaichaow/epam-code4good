285865b80fa18f6face150678d3bfc9713a44f04
BigApple07 <apple_liao@epam.com>
2018-11-17 15:59:53 +0800

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,65 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="ibox">
+      <div class="ibox-content">
+        <div class="ibox-img"><img src="/img/img_news_01.png" ></div>
+        <div class="ibox-text">
+          <div class="ibox-title">国际消除平困日活动</div>
+          <div class="ibox-note">
+            <span>2018/11/10</span>
+            <div class="have-people">
+              <img src="/img/icon_task_member@3x.png" > 12
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="ibox-box">
+        <a class="share" href="">
+          <img src="/img/icon_task_share@3x.png"> 分享给朋友
+        </a>
+        <button>申请加入</button>
+      </div>
+    </div>
+    <div class="ibox">
+      <div class="ibox-content">
+        <div class="ibox-img"><img src="/img/img_news_02.png" ></div>
+        <div class="ibox-text">
+          <div class="ibox-title">国际消除平困日活动</div>
+          <div class="ibox-note">
+            <span>2018/11/10</span>
+            <div class="have-people">
+              <img src="/img/icon_task_member@3x.png" > 12
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="ibox-box">
+        <a class="share" href="">
+          <img src="/img/icon_task_share@3x.png"> 分享给朋友
+        </a>
+        <button>申请加入</button>
+      </div>
+    </div>
+    <div class="ibox">
+      <div class="ibox-content">
+        <div class="ibox-img"><img src="/img/img_news_01.png" ></div>
+        <div class="ibox-text">
+          <div class="ibox-title">国际消除平困日活动</div>
+          <div class="ibox-note">
+            <span>2018/11/10</span>
+            <div class="have-people">
+              <img src="/img/icon_task_member@3x.png" > 12
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="ibox-box">
+        <a class="share" href="">
+          <img src="/img/icon_task_share@3x.png"> 分享给朋友
+        </a>
+        <button>申请加入</button>
+      </div>
+    </div>
   </div>
 </template>
+<style lang="sass" scoped src="./About.sass"></style>
